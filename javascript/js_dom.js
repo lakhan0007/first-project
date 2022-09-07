4bf19107fe72a953574ae9fe675956bedcbedc97
lakhan0007 <lakhansinghrathore2001@gmail.com>
2022-09-07 21:02:02 +0530

    bmb

--- javascript/js_dom.js
+++ javascript/js_dom.js
@@ -1,4 +1,19 @@
 var element;
-element = document;
+// element1 = document.doctype;
+// element2 = document.URL;
+// element3 = document.domain;
+// element4 = document.baseURI;
+
+
+// console.log(element1);
+// console.log(element2);
+// console.log(element3);
+// console.log(element4);
+
+// element = document.getElementById("footer");
+// element = document.getElementsByClassName("list");
+// element = document.getElementById("footer");
+
+element = document.getElementsByClassName("list")innerHTML;
 
 console.log(element);
