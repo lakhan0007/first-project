0da9d192ec2404a6e2d05ffabe20dcf67b247e0b
lakhan0007 <lakhansinghrathore2001@gmail.com>
2022-10-06 22:14:34 +0530

    adsf

--- bootstrap_tamplate/js_dom.js
+++ bootstrap_tamplate/js_dom.js
@@ -2,5 +2,8 @@
 
     var element;
 
-    element = document.getElementById("contant").innerHTML;
+    element = document.getElementById("header").setAttribute("style","border: 10px solid green; color: pink;");
+    element = document.getElementById("header").getAttribute("style");
     console.log(element);
+    console.log(typeof(element));
+    // document.write(element);
